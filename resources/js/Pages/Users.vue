48c17e673d915bd8fc62dc66c3069c75c942f92f
black-code1 <legrandmahamat1@gmail.com>
2022-08-11 09:31:02 +0100

    Layout Files

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,28 +1,27 @@
 <template>
-    <h1>Users</h1>
+    <Layout>
+        <h1 class="text-4xl font-bold">
+            Users
+        </h1>
 
-    <Nav/>
+        <div style="margin-top: 400px">
+            <p>The current time is {{time}}</p>
 
-    <div style="margin-top: 400px">
-        <p>The current time is {{time}}.</p>
-
-        <Link
-            href="/users"
-            class="text-blue-500"
-            preserve-scroll
-        >
-            Refresh
-        </Link>
-    </div>
+            <Link
+                href="/users"
+                class="text-blue-500"
+                preserve-scroll>
+                Refresh
+            </Link>
+        </div>
+    </Layout>
 </template>
 
 <script>
-import Nav from '../Shared/Nav'
-import {Link} from '@inertiajs/inertia-vue3'
+import Layout from '../Shared/Layout'
+import {Link} from "@inertiajs/inertia-vue3";
 export default{
-    components: {Nav, Link},
-
-    props: {time: String}
+    components: {Layout, Link}
 }
 </script>
 
